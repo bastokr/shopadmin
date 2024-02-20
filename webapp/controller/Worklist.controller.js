c2d0c4df3b9281c5956aaa383026906398f55cdb
skpark <parksk66@gmail.com>
2024-02-20 10:26:17 +0900

    first commit

---
+++ webapp/controller/Worklist.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	'./BaseController',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/Device',
+	'sap/ui/demo/toolpageapp/model/formatter'
+], function (BaseController, JSONModel, Device, formatter) {
+	"use strict";
+	return BaseController.extend("sap.ui.demo.toolpageapp.controller.Worklist", {
+		formatter: formatter,
+
+		onInit: function () {
+
+
+		}
+	});
+});
